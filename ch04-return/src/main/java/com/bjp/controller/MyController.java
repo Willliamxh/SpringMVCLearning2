2fdc93a4104d5b3ab38a99ad91d3f249a943b529
xuhan <821602702@qq.com>
2022-04-11 11:12:04 +0800

    返回一个Student对象（传给浏览器是Json模式）

--- ch04-return/src/main/java/com/bjp/controller/MyController.java
+++ ch04-return/src/main/java/com/bjp/controller/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -79,7 +80,34 @@ public class MyController {
         pw.println(json);
         pw.flush();
         pw.close();
+    }
+
+    /**
+     * 处理器方法返回一个Student，通过框架转为json，响应ajax请求
+     * @ResponseBody:
+     *    作用：把处理器方法返回对象转为json后，通过HttpServletResponse输出给浏览器。
+     *    位置：方法的定义上面。 和其它注解没有顺序的关系。
+     * 返回对象框架的处理流程：
+     *  1. 框架会把返回Student类型，调用框架的中集合ArrayList<HttpMessageConverter>中每个类（一共8个类）的canWrite()方法
+     *     检查那个HttpMessageConverter接口的实现类能处理Student类型的数据--（最终找到这个为真）MappingJackson2HttpMessageConverter
+     *
+     *  2.框架会调用实现类的write（）， MappingJackson2HttpMessageConverter的write()方法
+     *    把李四同学的student对象转为json， 调用Jackson的ObjectMapper实现转为json
+     *    contentType: application/json;charset=utf-8
+     *
+     *  3.框架会调用@ResponseBody把2的结果数据输出到浏览器， ajax请求处理完成
+     */
+    @RequestMapping(value = "/returnStudentJson.do")
+    @ResponseBody
+    public Student doStudentJsonObject(String name, Integer age) {
+        //调用service，获取请求结果数据 ， Student对象表示结果数据
+        Student student = new Student();
+        student.setName("李四同学");
+        student.setAge(20);
+        return student; // 会被框架转为json
 
     }
+
+
 
 }
